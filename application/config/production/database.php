71de4f3c9a977b3654e372e6055d4a97463ca19e
shanto <shantocv@gmail.com>
2014-06-05 23:43:44 +0530

    created database in server and configured accordingly the database.php in config/procution/database.php

--- application/config/production/database.php
+++ application/config/production/database.php
@@ -48,10 +48,10 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['hostname'] = 'web375.webfaction.com';
+$db['default']['username'] = 'shantocv';
+$db['default']['password'] = 'shantocv';
+$db['default']['database'] = 'codeignitercms';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = FALSE;
